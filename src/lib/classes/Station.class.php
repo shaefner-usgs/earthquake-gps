213b01ad374a1b4c8f6bd6bee77ab8d9ecea6c9b
Scott Haefner <shaefner@usgs.gov>
2016-02-02 11:14:02 -0800

    tweaks

--- src/lib/classes/Station.class.php
+++ src/lib/classes/Station.class.php
@@ -78,9 +78,15 @@ class Station {
   }
 
   private function _getPhotosLink ($path) {
-    $dir = '/home/www/data/GPS/stations/7.dir/7adl/photos';
-    $exists = file_exists('data/stations/7.dir/7adl/photos/full/7adl_20030916e.jpg');
-    return $exists;
+    $photos = [];
+    $dir = sprintf('%s/stations/%s.dir/%s/photos/screen',
+      $GLOBALS['DATA_DIR'],
+      substr($this->station, 0, 1),
+      $this->station
+    );
+    $dir_contents = scandir($dir, SCANDIR_SORT_DESCENDING);
+    $photos['screen'] = array_diff($dir_contents, array('..', '.'));
+    return $photos;
   }
 
   private function _getWeatherLink () {
