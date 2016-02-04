0b4baa378b8014da36e274c4d980e56b3054b7b9
Scott Haefner <shaefner@usgs.gov>
2016-02-04 15:20:45 -0800

    add getContents func

--- src/lib/functions/functions.inc.php
+++ src/lib/functions/functions.inc.php
@@ -1,6 +1,25 @@
 <?php
 
 /**
+ * Get directory contents (checks first if it exists and doesn't return .., .)
+ * @param $dir {String}
+ *		directory to scan
+ * @param $order {Integer} default is SCANDIR_SORT_DESCENDING
+ *
+ * @return $r {Array}
+ */
+function getContents ($dir, $order=SCANDIR_SORT_DESCENDING) {
+	$r = [];
+
+	if (file_exists($dir)) {
+		$contents = scandir($dir, $order);
+		$r = array_diff($contents, array('..', '.'));
+	}
+
+	return $r;
+}
+
+/**
  * Get a request parameter from $_GET or $_POST.
  *
  * @param $name {String}
