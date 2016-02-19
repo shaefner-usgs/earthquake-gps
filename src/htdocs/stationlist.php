a390c5be5dbc676298a5760426391f7c7b69694c
Scott Haefner <shaefner@usgs.gov>
2016-02-19 12:53:42 -0800

    initial commit

---
+++ src/htdocs/stationlist.php
@@ -0,0 +1,63 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'GPS Station List';
+  $HEAD = '';
+  $FOOT = '';
+
+  include '../conf/config.inc.php'; // app config
+  include '../lib/functions/functions.inc.php'; // app functions
+  include '../lib/classes/Db.class.php'; // db connector, queries
+  include 'template.inc.php';
+}
+
+$filter = safeParam('filter', 'a');
+
+$db = new Db();
+
+// Db query result: station list
+$rsStationList = $db->queryStationList($filter);
+
+// Create a more "friendly" array to loop over for creating html
+while ($row = $rsStationList->fetch(PDO::FETCH_ASSOC)) {
+  $station = $row['station'];
+  $networks[$station][] = $row['network'];
+  $stations[$station] = $networks[$station];
+}
+
+// Create html for station list
+$list_html = '<ul>';
+foreach ($stations as $station => $networks) {
+  $networks_html = '<ul>';
+  foreach ($networks as $network) {
+    $networks_html .= "<li>$network</li>";
+  }
+  $networks_html .= '</ul>';
+  $list_html .= sprintf('<li><h3>%s</h3>%s</li>',
+    strtoupper($station),
+    $networks_html
+  );
+}
+$list_html .= '</ul>';
+
+// Db query result: first char for all stations in db
+$rsStationChars = $db->queryStationChars();
+
+// Create html for navbar
+$nav_html = '<nav class="jumplist">';
+while ($row = $rsStationChars->fetch(PDO::FETCH_ASSOC)) {
+  $link_html = '<a href="%s/stationlist.php?filter=%s">%s</a>';
+  if ($row['alphanum'] === $filter) {
+    $link_html = '<a href="%s/stationlist.php?filter=%s"><strong>%s</strong></a>';
+  }
+  $nav_html .= sprintf($link_html,
+    $MOUNT_PATH,
+    $row['alphanum'],
+    $row['alphanum']
+  );
+}
+$nav_html .= '</nav>';
+
+// Render html
+print $nav_html;
+print $list_html;
