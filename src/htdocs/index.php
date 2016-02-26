4e260e9aef8f57d5228b5543a687458c52d41a42
Scott Haefner <shaefner@usgs.gov>
2016-02-26 15:06:07 -0800

    add content

--- src/htdocs/index.php
+++ src/htdocs/index.php
@@ -1,24 +1,43 @@
 <?php
 
-include_once '../conf/config.inc.php';
-
 if (!isset($TEMPLATE)) {
   $TITLE = 'GPS Data';
   $NAVIGATION = true;
   $HEAD = '';
   $FOOT = '';
 
-  include_once 'template.inc.php';
+  include '../conf/config.inc.php'; // app config
+  include '../lib/functions/functions.inc.php'; // app functions
+
+  $json = import_json(__DIR__ . '/_getNetworks.json.php');
+  $networks = json_decode($json, true);
+
+  include 'template.inc.php';
 }
 
 ?>
 
-<p>As part of the earthquake process, Earth's surface is being deformed as earthquake faults accumulate strain and slip or slowly creep over time. We use GPS to monitor this movement by measuring the precise position (within 5mm or less) of stations near active faults relative to each other. Months or years later, we occupy the same stations again. By determining how the stations have moved we calculate ground deformation.</p>
+<p>As part of the earthquake process, Earth's surface is being deformed as earthquake faults accumulate strain and slip or slowly creep over time. We use GPS to monitor this movement by measuring the precise position (within 5mm or less) of stations near active faults relative to each other. Months or years later, we occupy the same stations again. By determining how the stations have moved we calculate ground deformation. <a href="about.php">Read more</a> &raquo;</p>
 
-<img src="data/networks/CentralUS/arfy/nafixed/arfy.png" />
+<section>
+  <h2>View Stations by Network</h2>
 
-<h2>GPS Networks</h2>
+  <div class="map"></div>
 
-<div class="map"></div>
+  <p>Networks on this map</p>
+  <?php print_r($networks); ?>
+</section>
 
-<h3>Networks on this map</h3>
+<section>
+  <h2>View Stations Alphabetically</h2>
+  <p><a href="stationlist/">Station List</a></p>
+</section>
+
+<section>
+  <h2>Google Earth Files</h2>
+  <p>All stations surveyed since 1992:</p>
+  <ul>
+    <li><a href="data/networks/USGS_years_observed.kmz">Sorted by number of years occupied</a></li>
+    <li><a href="data/networks/USGS_campaign_data.kmz">Sorted by last year occupied</a></li>
+  </ul>
+</section>
