0d97a7203b39b868ef71ba89e9847d3fb059494e
Scott Haefner <shaefner@usgs.gov>
2016-03-03 16:05:45 -0800

    store user layer choices; todo: restore onload

--- src/htdocs/lib/Leaflet.RestoreView/leaflet.restoreview.js
+++ src/htdocs/lib/Leaflet.RestoreView/leaflet.restoreview.js
@@ -4,13 +4,16 @@
 
 var RestoreViewMixin = {
   restoreView: function () {
-    var storage,
+    var layers,
+        storage,
         view;
 
     storage = window.localStorage || {};
+    layers = JSON.parse(storage.mapLayers || '{"base":"", "add":[], "remove":[]}');
 
     // Save settings: setup listeners, store settings in localStorage
     if (!this.__initRestore) {
+      // map extent
       this.on('moveend', function () {
         if (!this._loaded) {
           return;  // Never access map bounds if view is not set.
@@ -22,8 +25,29 @@ var RestoreViewMixin = {
         };
         storage.mapView = JSON.stringify(view);
       }, this);
-      this.on('baselayerchange overlayadd overlayremove', function (e) {
-        console.log(e);
+
+      // map layers
+      this.on('baselayerchange', function (e) {
+        layers.base = e.name;
+        storage.mapLayers = JSON.stringify(layers);
+      }, this);
+      this.on('overlayadd', function (e) {
+        var add = layers.add.indexOf(e.name),
+            remove = layers.remove.indexOf(e.name);
+        if (add === -1) {
+          layers.add.push(e.name);
+        }
+        layers.remove.splice(remove, 1);
+        storage.mapLayers = JSON.stringify(layers);
+      }, this);
+      this.on('overlayremove', function (e) {
+        var add = layers.add.indexOf(e.name),
+            remove = layers.remove.indexOf(e.name);
+        if (remove === -1) {
+          layers.remove.push(e.name);
+        }
+        layers.add.splice(add, 1);
+        storage.mapLayers = JSON.stringify(layers);
       }, this);
 
       this.__initRestore = true;
@@ -31,7 +55,7 @@ var RestoreViewMixin = {
 
     // Restore settings
     try {
-      view = JSON.parse(storage.mapView || '');
+      view = JSON.parse(storage.mapView || '{}');
       this.setView(L.latLng(view.lat, view.lng), view.zoom, true);
       return true;
     }
