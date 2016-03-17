12098c5a81c890aaecd60ca6fb2c226625f1851b
Scott Haefner <shaefner@usgs.gov>
2016-03-17 15:48:51 -0700

    initial commit

---
+++ src/htdocs/js/network/index.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var L = require('leaflet'); // aliased in browserify.js
+
+// Leaflet plugins
+require('leaflet/MousePosition');
+require('leaflet/Restoreview');
+
+// Factories for creating map layers (returns e.g. "L.earthquakesLayer()")
+require('leaflet/DarkLayer');
+require('leaflet/EarthquakesLayer');
+require('leaflet/FaultsLayer');
+require('leaflet/GreyscaleLayer');
+require('leaflet/SatelliteLayer');
+require('leaflet/StationsLayer');
+require('leaflet/TerrainLayer');
+
+// Define map layers
+var dark = L.darkLayer(),
+    earthquakes = L.earthquakesLayer('_getEarthquakes.json.php'),
+    faults = L.faultsLayer(),
+    greyscale = L.greyscaleLayer(),
+    satellite = L.satelliteLayer(),
+    stations = L.stationsLayer('_getStations.json.php'),
+    terrain = L.terrainLayer(),
+
+    baseLayers = {
+      'Greyscale': greyscale,
+      'Terrain': terrain,
+      'Satellite': satellite,
+      'Dark': dark
+    },
+    overlays = {
+      'Stations': stations,
+      'Earthquakes': earthquakes,
+      'Faults': faults
+    };
+
+// Create map
+var map = L.map(document.querySelector('.map'), {
+  center: [38, -123],
+  layers: [greyscale, earthquakes, faults],
+  scrollWheelZoom: false,
+  zoom: 9
+});
+
+// Add controllers
+L.control.layers(baseLayers, overlays).addTo(map);
+L.control.mousePosition().addTo(map);
+L.control.scale().addTo(map);
+
+// Remember user's map settings (selected layers, map extent)
+map.restoreView({
+  baseLayers: baseLayers,
+  overlays: overlays
+});
