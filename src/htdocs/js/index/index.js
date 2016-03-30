53bc89fb5793a2845356e52223a72ef7e9c5403d
Scott Haefner <shaefner@usgs.gov>
2016-03-30 08:29:31 -0700

    add networks layer; use coding standards

--- src/htdocs/js/index/index.js
+++ src/htdocs/js/index/index.js
@@ -1,48 +1,124 @@
 'use strict';
 
-var L = require('leaflet'); // aliased in browserify.js
+var L = require('leaflet'), // aliased in browserify.js
+    Xhr = require('util/Xhr');
 
 // Leaflet plugins
+require('leaflet-fullscreen');
+require('leaflet/MousePosition');
 require('leaflet/Restoreview');
 
 // Factories for creating map layers (returns e.g. "L.earthquakesLayer()")
 require('leaflet/DarkLayer');
 require('leaflet/GreyscaleLayer');
+require('leaflet/NetworksLayer.js');
 require('leaflet/SatelliteLayer');
 require('leaflet/TerrainLayer');
 
-// Define map layers
-var dark = L.darkLayer(),
-    greyscale = L.greyscaleLayer(),
-    satellite = L.satelliteLayer(),
-    terrain = L.terrainLayer(),
+var IndexMap = function () {
+  var _this,
+      _initialize,
 
-    baseLayers = {
+      _networks,
+
+      _getMapLayers,
+      _getNetworksLayer,
+      _initMap;
+
+  _this = {};
+
+  _initialize = function () {
+    // Get netwoks layer and call initMap() when finished
+    _getNetworksLayer();
+  };
+
+  /**
+   * Get all map layers that will be displayed on map
+   *
+   * @return layers {Object} {
+   *   baseLayers: {Object}
+   *   overlays: {Object}
+   *   defaults: {Array}
+   * }
+   */
+  _getMapLayers = function () {
+    var dark,
+        greyscale,
+        layers,
+        satellite,
+        terrain;
+
+    dark = L.darkLayer();
+    greyscale = L.greyscaleLayer();
+    satellite = L.satelliteLayer();
+    terrain = L.terrainLayer();
+
+    layers = {};
+    layers.baseLayers = {
       'Greyscale': greyscale,
       'Terrain': terrain,
       'Satellite': satellite,
       'Dark': dark
-    },
-    overlays = {
-
     };
+    layers.overlays = {
+      'Networks': _networks
+    };
+    layers.defaults = [terrain, _networks];
 
-// Create map
-var map = L.map(document.querySelector('.map'), {
-  center: [38, -123],
-  layers: [greyscale],
-  scrollWheelZoom: false,
-  zoom: 9
-});
+    return layers;
+  };
 
-// Add controllers
-L.control.layers(baseLayers, overlays).addTo(map);
-L.control.scale().addTo(map);
+  /**
+   * Get networks layer from geojson data via ajax
+   */
+  _getNetworksLayer = function () {
+    Xhr.ajax({
+      url: '_getNetworks.json.php',
+      success: function (data) {
+        _networks = L.networksLayer(data);
+        _initMap();
+      },
+      error: function (status) {
+        console.log(status);
+      }
+    });
+  };
 
-// Remember user's map settings (selected layers, map extent)
-map.restoreView({
-  baseLayers: baseLayers,
-  id: 'network-map',
-  overlays: overlays,
-  shareLayers: true
-});
+  /**
+   * Create Leaflet map instance
+   */
+  _initMap = function () {
+    var bounds,
+        layers;
+
+    layers = _getMapLayers();
+
+    // Create map
+    var map = L.map(document.querySelector('.map'), {
+      layers: layers.defaults,
+      scrollWheelZoom: false
+    });
+
+    // Set intial map extent to contain networks overlay
+    bounds = _networks.getBounds();
+    map.fitBounds(bounds);
+
+    // Add controllers
+    L.control.layers(layers.baseLayers, layers.overlays).addTo(map);
+    L.control.scale().addTo(map);
+
+    // Remember user's map settings (selected layers, map extent)
+    map.restoreView({
+      baseLayers: layers.baseLayers,
+      id: 'networks',
+      overlays: layers.overlays,
+      shareLayers: false
+    });
+  };
+
+  _initialize();
+
+  return _this;
+};
+
+IndexMap();
