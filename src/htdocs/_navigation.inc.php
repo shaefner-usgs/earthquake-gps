ab29f40360dfffcf706414004bcd764179b127e1
Scott Haefner <shaefner@usgs.gov>
2016-04-18 08:40:56 -0700

    chnage nav to group; match multiple pages

--- src/htdocs/_navigation.inc.php
+++ src/htdocs/_navigation.inc.php
@@ -1,8 +1,23 @@
 <?php
 
+$section = '/monitoring/gps';
+
+$matches = false;
+if (preg_match("@^/monitoring/gps/?(index.php)?$@", $_SERVER['REQUEST_URI']) ||
+  $_SERVER['REQUEST_URI'] === "$section/kinematic.php" ||
+  $_SERVER['REQUEST_URI'] === "$section/network.php" ||
+  $_SERVER['REQUEST_URI'] === "$section/qc.php" ||
+  $_SERVER['REQUEST_URI'] === "$section/station.php" ||
+  $_SERVER['REQUEST_URI'] === "$section/photos.php" ||
+  $_SERVER['REQUEST_URI'] === "$section/logsheets.php") {
+    $matches = true;
+}
+
 $NAVIGATION =
-  navItem('/', 'GPS Data') .
-  navItem('/stations/', 'Station List') .
-  navItem('/about.php', 'About GPS');
+  navGroup('GPS',
+    navItem("$section/", 'Data', $matches) .
+    navItem("$section/stations/", 'Station List') .
+    navItem("$sectionabout.php", 'About')
+  );
 
 print $NAVIGATION;
