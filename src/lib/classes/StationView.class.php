3c9daed9e5e717f006ba37fa5a04e4df6e7144e2
Scott Haefner <shaefner@usgs.gov>
2016-04-20 16:09:20 -0700

    add disclaimer

--- src/lib/classes/StationView.class.php
+++ src/lib/classes/StationView.class.php
@@ -13,17 +13,25 @@ class StationView {
     $this->model = $model;
   }
 
-  private function _getCampaignListMarkup () {
+  private function _getCampaignList () {
     return '<h2>Campaign List</h2>';
   }
 
-  private function _getStationDetailsMarkup () {
+  private function _getStationDetails () {
     return '<h2>Station Details</h2>';
   }
 
+  private function _getDisclaimer () {
+    return '<p><small>These results are preliminary. The station positions are
+      unchecked and should not be used for any engineering applications. There
+      may be errors in the antenna heights. The velocities are very dependent
+      on the length of the span of observations. The presence of outliers
+      (errant observations) sometimes contaminates the velocities.</small></p>';
+  }
+
   public function render () {
-    print $this->_getStationDetailsMarkup();
-    print $this->_getCampaignListMarkup();
+    print $this->_getStationDetails();
+    print $this->_getCampaignList();
 
     // campaign station -> photos
     // continuous station -> kinematic plots
@@ -31,5 +39,7 @@ class StationView {
     print '<pre>';
     print var_dump($this->model);
     print '</pre>';
+
+    print $this->_getDisclaimer();
   }
 }
