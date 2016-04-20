340aeda7bedf30c69b25463938925a2b8482617e
Scott Haefner <shaefner@usgs.gov>
2016-04-20 09:38:13 -0700

    add css, subheader, change source order

--- src/htdocs/stationlist.php
+++ src/htdocs/stationlist.php
@@ -3,7 +3,7 @@
 if (!isset($TEMPLATE)) {
   $TITLE = 'GPS Station List';
   $NAVIGATION = true;
-  $HEAD = '';
+  $HEAD = '<link rel="stylesheet" href="/css/stationlist/index.css" />';
   $FOOT = '';
 
   include '../conf/config.inc.php'; // app config
@@ -12,24 +12,53 @@ if (!isset($TEMPLATE)) {
   include 'template.inc.php';
 }
 
-$filter = safeParam('filter', 'a');
+$filter = safeParam('filter');
 
 $db = new Db();
+
+// Db query result: first char for all stations in db
+$rsStationChars = $db->queryStationChars();
+
+// Create html for jumplist
+$nav_html = '<nav class="jumplist">';
+while ($row = $rsStationChars->fetch(PDO::FETCH_ASSOC)) {
+  $link_html = '<a href="%s/stations/%s/">%s</a>';
+  // highlight current selection
+  if ($row['alphanum'] === $filter) {
+    $link_html = '<a href="%s/stations/%s/"><strong>%s</strong></a>';
+  }
+  $nav_html .= sprintf($link_html,
+    $MOUNT_PATH,
+    $row['alphanum'],
+    strtoupper($row['alphanum'])
+  );
+}
+$nav_html .= '</nav>';
 
 // Db query result: station list
 $rsStationList = $db->queryStationList($filter);
 
-// Create a more "friendly" array to loop over for creating html
+// Create a more "friendly" array to loop over for creating list html
 while ($row = $rsStationList->fetch(PDO::FETCH_ASSOC)) {
   $station = $row['station'];
   $networks[$station][] = $row['network'];
   $stations[$station] = $networks[$station];
 }
 
+// Create html for subheader
+$sel = strtoupper($filter);
+if (!$sel) {
+  $sel = 'All';
+}
+$subheader = sprintf ('<h2>&lsquo;%s&rsquo; Stations (%d)</h2>',
+  $sel,
+  count($stations)
+);
+
 // Create html for station list
-$list_html = '<ul class="no-style">';
+$list_html = '<ul class="stations no-style">';
 foreach ($stations as $station => $networks) {
-  $networks_html = '<ul>';
+  $networks_html = '<ul class="no-style">';
   foreach ($networks as $network) {
     $networks_html .= sprintf ('<li><a href="%s/%s/%s/">%s</a></li>',
       $MOUNT_PATH,
@@ -46,24 +75,7 @@ foreach ($stations as $station => $networks) {
 }
 $list_html .= '</ul>';
 
-// Db query result: first char for all stations in db
-$rsStationChars = $db->queryStationChars();
-
-// Create html for jumplist
-$nav_html = '<nav class="jumplist">';
-while ($row = $rsStationChars->fetch(PDO::FETCH_ASSOC)) {
-  $link_html = '<a href="%s/stations/%s/">%s</a>';
-  if ($row['alphanum'] === $filter) {
-    $link_html = '<a href="%s/stations/%s/"><strong>%s</strong></a>';
-  }
-  $nav_html .= sprintf($link_html,
-    $MOUNT_PATH,
-    $row['alphanum'],
-    strtoupper($row['alphanum'])
-  );
-}
-$nav_html .= '</nav>';
-
 // Render html
 print $nav_html;
+print $subheader;
 print $list_html;
