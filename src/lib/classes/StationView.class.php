0aeef7bf106b61065d494cf72b12cedd8fc7c66a
Scott Haefner <shaefner@usgs.gov>
2016-04-21 16:23:58 -0700

    add _getPlots method

--- src/lib/classes/StationView.class.php
+++ src/lib/classes/StationView.class.php
@@ -1,5 +1,7 @@
 <?php
 
+include '../conf/config.inc.php'; // app config
+
 /**
  * Station view
  * - creates the HTML for station.php
@@ -17,10 +19,6 @@ class StationView {
     return '<h2>Campaign List</h2>';
   }
 
-  private function _getStationDetails () {
-    return '<h2>Station Details</h2>';
-  }
-
   private function _getDisclaimer () {
     return '<p><small>These results are preliminary. The station positions are
       unchecked and should not be used for any engineering applications. There
@@ -29,16 +27,99 @@ class StationView {
       (errant observations) sometimes contaminates the velocities.</small></p>';
   }
 
+  private function _getMap () {
+    return '<div class="map"></div>';
+  }
+
+  private function _getPlots () {
+    $explanation = '
+      <p>These plots depict the north, east and up components of
+      the station as a function of time. <a href="/monitoring/gps/plots.php">More
+      detailed explanation</a> &raquo;</p>
+      <p>Dashed lines show offsets due to:</p>
+      <ul class="no-style">
+        <li><mark class="green">Green</mark>: antenna changes from site logs</li>
+        <li><mark class="red">Red</mark>: earthquakes</li>
+        <li><mark class="blue">Blue</mark>: manually entered</li>
+      </ul>';
+
+    $types = [
+      'itrf2008' => 'ITRF2008',
+      'nafixed' => 'NA-fixed',
+      'filtered' => 'Filtered NA-fixed',
+      'cleaned' => 'Detrended'
+    ];
+
+    $html = '<div class="tablist">';
+
+    foreach ($types as $type => $name) {
+      $baseDir = $GLOBALS['DATA_DIR'];
+      $baseUri = $GLOBALS['MOUNT_PATH'] . '/data';
+      $imgPath = 'networks/' . $this->model->network . '/' .
+        $this->model->station . '/' . $type;
+      $file = $this->model->station . '.png';
+      $nav = '';
+
+      if (is_file("$baseDir/$imgPath/$file")) {
+        $imgSrc = "$baseUri/$imgPath/$file";
+        if ($type === 'cleaned') {
+          $imgSrc30 = str_replace('.png', '_30.png', $imgSrc);
+          $imgSrc90 = str_replace('.png', '_90.png', $imgSrc);
+          $imgSrc365 = str_replace('.png', '_365.png', $imgSrc);
+          $imgSrc730 = str_replace('.png', '_730.png', $imgSrc);
+          $nav = '
+            <ul>
+              <li><a href="' . $imgSrc30 . '">Past 30 days</a></li>
+              <li><a href="' . $imgSrc90 . '">Past 90 days</a></li>
+              <li><a href="' . $imgSrc365 . '">Past year</a></li>
+              <li><a href="' . $imgSrc730 . '">Past 2 years</a></li>
+              <li><a href="' . $imgSrc . '">All data</a></li>
+            </ul>';
+        }
+        $html .= sprintf('
+          <section class="panel" data-title="%s">
+            <header>
+              <h3>%s</h3>
+            </header>
+            %s
+            <img src="%s" alt="Plot showing %s data" />
+            %s
+          </section>',
+          $name,
+          $name,
+          $nav,
+          $imgSrc,
+          $name,
+          $explanation
+        );
+      }
+    }
+    $html .= '</div>';
+
+    return $html;
+  }
+
+  private function _getStationDetails () {
+    return '<h2>Station Details</h2>';
+  }
+
   public function render () {
-    print $this->_getStationDetails();
-    print $this->_getCampaignList();
+
+    // print '<section class="row">';
+    //
+    // print $this->_getStationDetails();
+    // print $this->_getCampaignList();
+    //
+    // print '</section>';
+
+    print $this->_getPlots();
 
     // campaign station -> photos
     // continuous station -> kinematic plots
 
-    print '<pre>';
-    print var_dump($this->model);
-    print '</pre>';
+    // print '<pre>';
+    // print var_dump($this->model);
+    // print '</pre>';
 
     print $this->_getDisclaimer();
   }
