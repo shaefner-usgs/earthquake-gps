a34c8a4b94bd9410253123313fafb5eb61701833
Scott Haefner <shaefner@usgs.gov>
2016-04-22 12:08:14 -0700

    implement tablist methods here b/c the native methods wipe out eventlisteners

--- src/htdocs/js/station/index.js
+++ src/htdocs/js/station/index.js
@@ -6,8 +6,8 @@ var Plots = function (el) {
   var _this,
       _initialize,
 
-      _togglePlot,
-      _addListeners;
+      _addListeners,
+      _togglePlot;
 
   _this = {};
 
@@ -29,12 +29,53 @@ var Plots = function (el) {
 
     e.preventDefault();
     img = el.parentNode.querySelector('.toggle');
-    img.setAttribute('src', e.srcElement.href);
+    img.setAttribute('src', e.target.href);
   };
 
   _initialize();
+
+  return _this;
 };
 
 Plots(document.querySelector('.nav-toggle'));
 
-TabList.tabbifyAll();
+//TabList.tabbifyAll();
+
+// Temporary hack: implement these methods here b/c class method tabbifyOne
+// is wiping out my event listeners
+
+var tabbifyOne = function (el) {
+  var tabs = [],
+      panels,
+      panel,
+      i, len,
+      tablist;
+
+  panels = el.querySelectorAll('.panel');
+  for (i = 0, len = panels.length; i < len; i++) {
+    panel = panels[i];
+    tabs.push({
+      'title': panel.getAttribute('data-title') ||
+          panel.querySelector('header').innerHTML,
+      'content': panel,
+      'selected': panel.getAttribute('data-selected') === 'true'
+    });
+  }
+
+  tablist = TabList({
+    'tabs': tabs
+  });
+
+  el.parentNode.replaceChild(tablist.el, el);
+};
+
+var tabbifyAll = function () {
+  var lists,
+      i;
+  lists = document.querySelectorAll('.tablist');
+  for (i = lists.length - 1; i >= 0; i--) {
+    tabbifyOne(lists[i]);
+  }
+};
+
+tabbifyAll();
