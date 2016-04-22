01d07817233e7f55c67de3e882b27e075d57ff8c
Scott Haefner <shaefner@usgs.gov>
2016-04-22 09:55:18 -0700

    add toggle for plots

--- src/htdocs/js/station/index.js
+++ src/htdocs/js/station/index.js
@@ -2,4 +2,30 @@
 
 var TabList = require('tablist/TabList');
 
+var initNavToggle,
+    togglePlot;
+
+togglePlot = function (e) {
+  var img;
+
+  e.preventDefault();
+  img = document.querySelector('.toggle');
+  img.setAttribute('src', e.srcElement.href);
+};
+
+initNavToggle = function () {
+  var i,
+      navToggleAs;
+
+  navToggleAs = document.querySelectorAll('.nav-toggle a');
+  //[].slice.call(navToggleAs).forEach(function(a) {
+  for (i = 0; i < navToggleAs.length; i ++) {
+    console.log(navToggleAs[i]);
+    navToggleAs[i].addEventListener('click', togglePlot);
+  }
+  //});
+};
+
+initNavToggle();
+
 TabList.tabbifyAll();
