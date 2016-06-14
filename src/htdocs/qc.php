791ddb5e378124c8c021f9bbcfc82b9a2144098f
Scott Haefner <shaefner@usgs.gov>
2016-06-14 12:32:34 -0700

    update links to js, css files

--- src/htdocs/qc.php
+++ src/htdocs/qc.php
@@ -6,14 +6,14 @@ if (!isset($TEMPLATE)) {
   $TITLE = 'Quality Control Plots';
   $NAVIGATION = true;
   $HEAD = '
-    <link rel="stylesheet" href="lib/c3/c3.css" />
-    <link rel="stylesheet" href="css/qc.css" />
+    <link rel="stylesheet" href="/lib/c3/c3.css" />
+    <link rel="stylesheet" href="' . $MOUNT_PATH . '/css/qc.css" />
   ';
   $FOOT = '
     <script>var MOUNT_PATH = "' . $MOUNT_PATH . '";</script>
-    <script src="lib/d3/d3.js"></script>
-    <script src="lib/c3/c3.js"></script>
-    <script src="js/qc.js"></script>
+    <script src="/lib/d3/d3.js"></script>
+    <script src="/lib/c3/c3.js"></script>
+    <script src="' . $MOUNT_PATH . '/js/qc.js"></script>
   ';
 
   include_once 'template.inc.php';
