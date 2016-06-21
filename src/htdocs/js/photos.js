d9232ebafde93898f4354da25a2105b29754d971
Scott Haefner <shaefner@usgs.gov>
2016-06-21 16:03:47 -0700

    add simplbox options

--- src/htdocs/js/photos.js
+++ src/htdocs/js/photos.js
@@ -1,6 +1,31 @@
 /* global SimplBox */
+'use strict';
+
 
 var thumbs = document.querySelectorAll('[data-simplbox]');
 var simplbox = new SimplBox(thumbs);
 
-simplbox.init();
+console.log(simplbox);
+
+/* jshint ignore:start */
+simplbox.init({
+  imageSize: 0.8,
+  quitOnDocumentClick: true,
+  onStart: function () {
+      overlayOn();
+      closeButtonOn(simplbox);
+  },
+  onEnd: function () {
+      overlayOff();
+      closeButtonOff();
+      captionOff();
+  },
+  onImageLoadStart: function () {
+      preLoadIconOn();
+      captionOn(simplbox);
+  },
+  onImageLoadEnd: function () {
+      preLoadIconOff();
+  }
+});
+/* jshint ignore:end */
