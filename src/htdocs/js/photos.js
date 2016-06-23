59577027736d86cb812d6173d1528b6b559b6ab7
Scott Haefner <shaefner@usgs.gov>
2016-06-23 10:51:00 -0700

    set options, styles for simplbox

--- src/htdocs/js/photos.js
+++ src/htdocs/js/photos.js
@@ -1,16 +1,67 @@
-/* global SimplBox */
+///* global SimplBox */
 'use strict';
+
+/* jshint ignore:start */
+var preLoadIconOn = function () {
+          var div1 = document.createElement('div'),
+              div2 = document.createElement('div');
+              div1.setAttribute('id', 'simplbox-loading');
+              div1.appendChild(div2);
+          document.body.appendChild(div1);
+      },
+      preLoadIconOff = function () {
+          var el = document.getElementById('simplbox-loading');
+          el.parentNode.removeChild(el);
+      },
+      overlayOn = function () {
+          var div = document.createElement('div');
+          div.setAttribute('id', 'simplbox-overlay');
+          document.body.appendChild(div);
+      },
+      overlayOff = function () {
+          var el = document.getElementById('simplbox-overlay');
+          el.parentNode.removeChild(el);
+      },
+      closeButtonOn = function (base) {
+          var div = document.createElement('div');
+          div.setAttribute('id', 'simplbox-close');
+          document.body.appendChild(div);
+          div = document.getElementById('simplbox-close');
+          base.API_AddEvent(div, 'click touchend', function () {
+              base.API_RemoveImageElement();
+              return false;
+          });
+      },
+      closeButtonOff = function () {
+          var el = document.getElementById('simplbox-close');
+          el.parentNode.removeChild(el);
+      },
+      captionOn = function (base) {
+          var documentFragment = document.createDocumentFragment(),
+              newElement = document.createElement('div'),
+              newText = document.createTextNode(base.m_Alt);
+
+          captionOff();
+
+          newElement.setAttribute('id', 'simplbox-caption');
+          newElement.appendChild(newText);
+          documentFragment.appendChild(newElement);
+          document.body.appendChild(documentFragment);
+      },
+      captionOff = function () {
+          var el = document.getElementById('simplbox-caption');
+          if (el) {
+            el.parentNode.removeChild(el);
+          }
+      };
 
 
 var thumbs = document.querySelectorAll('[data-simplbox]');
-var simplbox = new SimplBox(thumbs);
-
-console.log(simplbox);
-
-/* jshint ignore:start */
-simplbox.init({
+var simplbox = new SimplBox(thumbs, {
   imageSize: 0.8,
   quitOnDocumentClick: true,
+  quitOnImageClick: false,
+  animationSpeed: 0,
   onStart: function () {
       overlayOn();
       closeButtonOn(simplbox);
@@ -28,4 +79,7 @@ simplbox.init({
       preLoadIconOff();
   }
 });
+
+simplbox.init();
+
 /* jshint ignore:end */
