0af3c27a98af1d03e9f4306e8762557985c3e021
Scott Haefner <shaefner@usgs.gov>
2016-07-11 15:00:24 -0700

    add data table

--- src/htdocs/qctable.php
+++ src/htdocs/qctable.php
@@ -10,7 +10,7 @@ $station = safeParam('station', 'p271');
 if (!isset($TEMPLATE)) {
   $TITLE = 'GPS Station ' . strtoupper($station) . ' Quality Control Data';
   $NAVIGATION = true;
-  $HEAD = '<link rel="stylesheet" href="../../../css/minimal.css" />';
+  $HEAD = '<link rel="stylesheet" href="../../../css/base.css" />';
   $CONTACT = 'jsvarc';
 
   include_once 'template.inc.php';
@@ -18,7 +18,34 @@ if (!isset($TEMPLATE)) {
 
 $db = new Db;
 
+// Db query result: past 14 records of QC data
 $rsQcData = $db->queryQcData($station, 14);
+
+// Create html for table body
+$table_body_html = '';
+while($row = $rsQcData->fetch(PDO::FETCH_ASSOC)) {
+  $table_body_html .= sprintf("<tr>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+      <td>%s</td>
+    </tr>\n",
+    substr($row['date'], 0, 4),
+    date('z', strtotime($row['date'])) + 1,
+    $row['date'],
+    $row['filename'],
+    $row['pos_obs'],
+    $row['comp_obs'],
+    $row['percentage'],
+    $row['mp1'],
+    $row['mp2']
+  );
+}
 
 $backLink = sprintf('%s/%s/%s',
   $MOUNT_PATH,
@@ -30,6 +57,23 @@ $name = strtoupper($station);
 ?>
 
 <h2>Past 14 observations</h2>
+
+<table>
+  <tr>
+    <th>Year</th>
+    <th><abbr title="Day of the year">DoY</abbr></th>
+    <th>Date</th>
+    <th>Filename</th>
+    <th>Pos Obs</th>
+    <th>Comp Obs</th>
+    <th>Percentage</th>
+    <th>MP1</th>
+    <th>MP2</th>
+  </tr>
+
+<?php print $table_body_html; ?>
+
+</table>
 
 <p class="back">&laquo;
   <a href="<?php print $backLink; ?>">Back to station <?php print $name; ?></a>
