7a54f074afcfb2f19ba4939406cda929f9f3191f
Scott Haefner <shaefner@usgs.gov>
2016-07-13 15:57:40 -0700

    add sort params

--- src/htdocs/kml.php
+++ src/htdocs/kml.php
@@ -4,8 +4,15 @@ include '../lib/_functions.inc.php'; // app functions
 include '../lib/classes/Kml.class.php'; // Waypoints class
 
 $network = safeParam('network', 'Pacific');
+$sortKey = safeParam('sortKey');
+
+$sort = [
+  'last' => 'last_obs',
+  'total' => 'total_years'
+];
 
 $kml = new Kml($network);
 
-//$kml->setPhpHeaders();
+$kml->sort($sort[$sortKey]);
+$kml->setPhpHeaders();
 $kml->render();
