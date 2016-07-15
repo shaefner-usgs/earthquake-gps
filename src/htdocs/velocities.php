5b23b92d23e2185bd8ff9e0e80f7da9f934fd6be
Scott Haefner <shaefner@usgs.gov>
2016-07-15 16:14:43 -0700

    add data to table

--- src/htdocs/velocities.php
+++ src/htdocs/velocities.php
@@ -7,9 +7,9 @@ include_once '../lib/classes/Db.class.php'; // db connector, queries
 $network = safeParam('network', 'SFBayArea');
 
 if (!isset($TEMPLATE)) {
-  $TITLE = "$network Network - Velocities and Uncertainties";
+  $TITLE = "$network Network";
   $NAVIGATION = true;
-  //$HEAD = '<link rel="stylesheet" href="../css/base.css" />';
+  $HEAD = '<link rel="stylesheet" href="../css/velocities.css" />';
   $CONTACT = 'jsvarc';
 
   include_once 'template.inc.php';
@@ -17,28 +17,63 @@ if (!isset($TEMPLATE)) {
 
 $db = new Db;
 
-// Db query result: past 14 records of QC data
+// Db query result: velocities for selected network
 $rsVelocities = $db->queryVelocities($network);
 
-// Create html for table body
-$table_body_html = '';
-while($row = $rsVelocities->fetch(PDO::FETCH_ASSOC)) {
-  $table_body_html .= sprintf("<tr>
-      <td>%s</td>
-      <td>%s</td>
-      <td>%s</td>
-      <td>%s</td>
-      <td>%s</td>
-      <td>%s</td>
-    </tr>\n",
-    $row['velocity'],
-    $row['sigma'],
-    $row['rms'],
-    $row['whitenoise'],
-    $row['randomwalk'],
-    $row['flickernoise']
-  );
+// Create velocities array from db result that is more friendly for parsing
+$velocities = $db->createVelocitiesArray($rsVelocities);
+
+$fields = [
+  'velocity',
+  'sigma',
+  'rms',
+  'whitenoise',
+  'randomwalk',
+  'flickernoise'
+];
+
+// Create html for table
+$table_html = '<table>';
+
+// Table header
+$th = '<tr><td class="empty"></td><th>Type</th>';
+foreach ($fields as $field) {
+  $name = $velocities['lookup'][$field];
+  $th .= "<th>$name</th>";
 }
+$th .= '</tr>';
+
+// Table body
+foreach ($velocities['data'] as $station => $types) {
+  // add station name and table header
+  $table_html .= '<tr class="station"><td colspan="8"><h3>Station ' .
+    strtoupper($station) . '</h3></td></tr>';
+  $table_html .= $th;
+
+  foreach ($types as $type => $components) {
+    if ($type !== 'filtered') { // don't show filtered data
+      foreach ($components as $direction => $data) {
+        // add component and type
+        $tr = "<tr><th>$direction</th>";
+        if ($direction === 'north') {
+          $tr .= '<td rowspan="3">' . $type . '</td>';
+        }
+        // add data fields (or '-' for no data)
+        foreach ($fields as $field) {
+          if (array_key_exists($field, $data)) {
+            $tr .= '<td>' . $data[$field] . '</td>';
+          } else {
+            $tr .=  '<td class="novalue">&ndash;</td>';
+          }
+        }
+        $tr .= "</tr>";
+        $table_html .= $tr;
+      }
+    }
+  }
+}
+
+$table_html .= '</table>';
 
 $backLink = sprintf('%s/%s',
   $MOUNT_PATH,
@@ -47,21 +82,9 @@ $backLink = sprintf('%s/%s',
 
 ?>
 
-<h2>Past 14 observations</h2>
+<h2>Velocities and Uncertainties</h2>
 
-<table>
-  <tr>
-    <th>Velocity (mm/yr)</th>
-    <th>Uncertainty (mm/yr)</th>
-    <th>RMS (mm)</th>
-    <th>White Noise</th>
-    <th>Random Walk</th>
-    <th>Flicker Noise</th>
-  </tr>
-
-<?php print $table_body_html; ?>
-
-</table>
+<?php print $table_html; ?>
 
 <p class="back">&laquo;
   <a href="<?php print $backLink; ?>">Back to <?php print $network; ?>network</a>
