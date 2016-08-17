6adb16c575bd6083b2690b81212290a7148c150e
Scott Haefner <shaefner@usgs.gov>
2016-08-17 09:26:57 -0700

    initial commit

---
+++ src/htdocs/cooperators.php
@@ -0,0 +1,138 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'GPS Cooperators';
+  $NAVIGATION = true;
+  $CONTACT = 'jsvarc';
+
+  include 'template.inc.php';
+}
+
+?>
+
+<h2>Agencies Operating Continuous (Permanent) GNSS Sites</h2>
+
+<p>Many agencies operate permanent GNSS sites for which we display the results
+  of positioning and time series analysis on our website or that we use for
+  reference frame adjustments.  The agency information is compiled from site
+  logs, RINEX headers, and metadata that are provided on individual
+  networks&rsquo; web pages.</p>
+
+<ul>
+  <li>Agenzia Spaziale Italiana - Telespazio</li>
+  <li>Arkansas State Highway and Transportation Department - U.S.A</li>
+  <li>Agenzia Spaziale Italiana</li>
+  <li>Badan Koordinasi Survei dan Pemetaan Nasional - Indonesia</li>
+  <li>Austria Federal Office of Metrology and Surveying</li>
+  <li>Bundesamt fuer Kartographie und Geodaesie (BKG) Geodetical Observatory Wettzell</li>
+  <li>California Institute of Technology</li>
+  <li>China Geo-informatics Center (NGCC)</li>
+  <li>Central Washington University</li>
+  <li>Centro de Investigacion Cientifica y de Educacion Superior de Ensenada - Mexico</li>
+  <li>City of Mountain Home - Arkansas U.S.A.</li>
+  <li>Centre National d&rsquo;Etudes Spatiales - France</li>
+  <li>Condor Earth Technology</li>
+  <li>Orange County Surveyors Office - California U.S.A.</li>
+  <li>Indonesian Center of Volcanology and Geological Hazard Mitigation</li>
+  <li>Washington State Department of Natural Resources - U.S.A.</li>
+  <li>Directorate General of Territorial Development - Portugal</li>
+  <li>Direction des Infrastructure de la Topographie et des Transport  Terrestres - New Caledonia</li>
+  <li>Technical University of Denmark National Space Institute</li>
+  <li>Delft University of Technology</li>
+  <li>Ecole Normale Superieure Centre National de la Recherche Scientifique</li>
+  <li>European Space Agency - European Space Operations Centre</li>
+  <li>U.S. Federal Aviation Administration</li>
+  <li>Finnish Geodetic Institute</li>
+  <li>The Ohio State University</li>
+  <li>Geoscience Australia</li>
+  <li>University of Memphis</li>
+  <li>Geospatial Information Authority of Japan</li>
+  <li>GFZ Potsdam</li>
+  <li>GNS Science - New Zealand</li>
+  <li>Natural Resources Canada - Geological Survey Canada</li>
+  <li>Hartebeesthoek Radio Astronomy Observatory - South Africa</li>
+  <li>Harvard-Smithsonian Center for Astrophysics</li>
+  <li>Brazilian Institute of Geography and Statistics</li>
+  <li>Instituto Geografico Nacional - Spain</li>
+  <li>Instituto Geografico Nacional - Guatemala</li>
+  <li>Instituto Nacional de Pesquisas Espaciais - Brazil</li>
+  <li>Instituto Nacional de Prevenci&cent;n S&deg;smica (INPRES) - Argentina</li>
+  <li>Institut de Physique du Globe de Paris</li>
+  <li>NASA  Jet Propulsion Laboratory</li>
+  <li>KMS Denmark - Danish Geodata Agency</li>
+  <li>Lamont-Doherty Earth Observatory</li>
+  <li>Lantmateriet Sweden</li>
+  <li>Louisiana State University</li>
+  <li>Main Astronomical Observatory National Academy of Sciences - Ukraine</li>
+  <li>Philippine National Mapping and Resource Information Authority</li>
+  <li>Nevada Bureau of Mines and Geology</li>
+  <li>National Geospatial Information - South Africa</li>
+  <li>U.S. National Ocean and Atmospheric Administration - National Geodetic Survey</li>
+  <li>Norwegian Mapping Authority Geodetic Institute</li>
+  <li>Natural Resources Canada</li>
+  <li>Natural Environment Research Council - United Kingdom</li>
+  <li>U.S. National Ocean and Atmospheric Administration -  Earth System Research Laboratory</li>
+  <li>Orange County Department of Public Works - California U.S.A.</li>
+  <li>Oregon Department of Transportation - U.S.A.</li>
+  <li>UNAVCO</li>
+  <li>Petroleum Development Oman</li>
+  <li>University of Hawaii at Manoa</li>
+  <li>Russian Data Analysis and Archive Center</li>
+  <li>Real Instituto y Observatorio de la Armada - Spain</li>
+  <li>Royal Observatory of Belgium</li>
+  <li>San Diego County Department of Public Works - California U.S.A.</li>
+  <li>Scripps Institution of Oceanography</li>
+  <li>Survey of Israel</li>
+  <li>Space Research Centre of the Polish Academy of Sciences</li>
+  <li>Swiss Federal Office of Topography</li>
+  <li>Institute of Metrology for Time and Space - VNIIFTRI - Russia</li>
+  <li>Tibet Autonomous Regional Bureau of Surveying and Mapping</li>
+  <li>U.S. Army Corps of Engineers</li>
+  <li>U.S. Coast Guard</li>
+  <li>U.S. Geological Survey Earthquake Hazards Program</li>
+  <li>University of California Berkeley</li>
+  <li>University Corporation for Atmospheric Research</li>
+  <li>U.S. Geological Survey Volcano Hazards Program</li>
+  <li>U.S. Naval Observatory</li>
+  <li>Russian Research Institute for Physical-technical and Radiotechnical Measurements</li>
+  <li>Warsaw University of Technology</li>
+  <li>California Department of Transportation - U.S.A.</li>
+  <li>University of Alaska Fairbanks Geophysical Institute</li>
+  <li>University of Alaska Fairbanks Geophysical Institute</li>
+</ul>
+
+<h2>Agencies Operating Campaign (Temporary) GNSS Sites</h2>
+
+<p>The majority of campaign GNSS site positions displayed on our website
+  were obtained from data collected by the USGS Earthquake Hazards and
+  Volcano Hazards Programs.  In some cases we include data collected by
+  other agencies for benchmarks that we also survey and data resulting
+  from collaborations between USGS and other groups.  We compile this
+  information from RINEX headers and site logs.</p>
+
+<ul>
+  <li>Arizona State University</li>
+  <li>California Department of Transportation</li>
+  <li>California Institute of Technology</li>
+  <li>Central Washington University</li>
+  <li>Humboldt State University</li>
+  <li>Louisiana State University</li>
+  <li>Massachusetts Institute of Technology</li>
+  <li>National Geodetic Survey</li>
+  <li>Nevada Department of Transportation</li>
+  <li>Oregon Institute of Technology</li>
+  <li>San Francisco State University</li>
+  <li>Scripps Institute of Oceanography</li>
+  <li>Smithsonian Astrophysical Observatory</li>
+  <li>Stanford University</li>
+  <li>University of Alaska, Fairbanks</li>
+  <li>University of California, Berkeley</li>
+  <li>University of California, Davis</li>
+  <li>University of California, Los Angeles</li>
+  <li>University of California, Riverside</li>
+  <li>University of California, Santa Cruz</li>
+  <li>University of Miami Rosenthiel School of Marine and Atmospheric Science</li>
+  <li>University of Southern California</li>
+  <li>U.S. Geological Survey Earthquake Hazards Program</li>
+  <li>U.S. Geological Survey Volcano Hazards Program</li>
+</ul>
