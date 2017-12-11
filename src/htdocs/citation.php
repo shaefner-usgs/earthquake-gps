8ca37f399cabef58e91b6d112d883e51fb72b3f3
Scott Haefner <shaefner@usgs.gov>
2017-12-11 14:40:07 -0800

    add citation page and update links

---
+++ src/htdocs/citation.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+  $TITLE = 'GPS Data Citation';
+  $NAVIGATION = true;
+  $CONTACT = 'jsvarc';
+
+  include 'template.inc.php';
+}
+
+?>
+
+<p>Campaign GPS data collected by the EHP for earthquake and tectonic studies
+  have provided input for many <a href="pdf/USGS-SGPS-refs.pdf">publications</a>.
+  We request that those using these data in their published results inform us
+  of the the citation for inclusion in this list by contacting
+  <a href="mailto:jrmurray@usgs.gov">Jessica Murray</a>. Raw and RINEX data
+  files for the campaign measurements collected by our group may be obtained
+  through the <a href="http://www.ncedc.org/survey-gps/">NCEDC</a>. Please
+  cite these data using doi:10.5066/F7NG4NRK</p>
+<p>If using the processed positions or results of time series analysis available
+  on this website, please cite Murray, J.R. and Svarc, J. (2017),
+  <a href="https://pubs.geoscienceworld.org/ssa/srl/article/88/3/916/284075/global-positioning-system-data-collection">Global
+  Positioning System data collection, processing, and analysis conducted by the
+  U.S. Geological Survey Earthquake Hazards Program</a>, Seismol. Res. Lett.,
+  doi:10.1785/0220160204, which provides a detailed description of our
+  methodology.</p>
