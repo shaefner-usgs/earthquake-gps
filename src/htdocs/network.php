cccd7c4c4025f76eb158858e46fe09d83b10ed13
Scott Haefner <shaefner@usgs.gov>
2019-02-13 14:25:42 -0800

    add note about pin color

--- src/htdocs/network.php
+++ src/htdocs/network.php
@@ -146,6 +146,7 @@ $geFiles_html = "$geFilesHeader<ul>$geFiles_lis</ul>";
 <section>
   <div class="map"></div>
   <?php print $legend_html; ?>
+  <p class="small">Pin color indicates when station was last updated.</p>
   <h3 class="count"><?php print $stations['count']; ?> Stations on this Map</h3>
   <?php
     print $stations_html;
